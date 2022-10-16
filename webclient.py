7104a621f1a91327d0db3beb5a2fd1dbd76c57da
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-10-15 20:39:53 -0700

    Copy given code

--- webclient.py
+++ webclient.py
@@ -1,15 +1,16 @@
-import socket  
-import sys 
+# import socket  
+# import sys 
 
-#Request from the client
-request = "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n" 
+# #Request from the client
+# request = "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n" 
 
-s = socket.socket()
-s.connect(("example.com",80))
-s.sendall(request.encode("ISO-8859-1"))
-data = s.recv(4096).decode("ISO-8859-1")
-while len(data) > 0:
-    print(sys.argv, data)
-    print(sys.argv[1])
-    data = s.recv(4096)
-s.close()
+# s = socket.socket()
+# s.connect(("example.com",80))
+# s.sendall(request.encode("ISO-8859-1"))
+# data = s.recv(4096).decode("ISO-8859-1")
+# while len(data) > 0:
+#     print(sys.argv, data)
+#     print(sys.argv[1])
+#     data = s.recv(4096)
+# s.close()
+
--- webserver.py
+++ webserver.py
@@ -1,45 +1,45 @@
-import socket  
-import sys 
-import os
-extensions = {'.txt':'text/plain', '.html':'text/html'}
+# import socket  
+# import sys 
+# import os
+# extensions = {'.txt':'text/plain', '.html':'text/html'}
 
-s = socket.socket()
-s.bind(('', int(sys.argv[1])))
-s.listen()
+# s = socket.socket()
+# s.bind(('', int(sys.argv[1])))
+# s.listen()
 
-def getData(data):
-    encode_data = data.recv(4096)
+# def getData(data):
+#     encode_data = data.recv(4096)
     
-    req = ""
-    while True:
-        decoded_req = encode_data.decode("ISO-8859-1")
-        req = req + decoded_req
+#     req = ""
+#     while True:
+#         decoded_req = encode_data.decode("ISO-8859-1")
+#         req = req + decoded_req
 
-        if decoded_req.find('\r\n\r\n'):
-            return req
+#         if decoded_req.find('\r\n\r\n'):
+#             return req
 
-while True:
+# while True:
 
-    new_conn = s.accept()
-    new_socket = new_conn[0]  
-    header_data = getData(new_socket) 
-    get = header_data.split("\r\n")[0].split()
+#     new_conn = s.accept()
+#     new_socket = new_conn[0]  
+#     header_data = getData(new_socket) 
+#     get = header_data.split("\r\n")[0].split()
     
-    req_method = get[0]
-    req_path = get[1]
-    req_protocol = get[2]
+#     req_method = get[0]
+#     req_path = get[1]
+#     req_protocol = get[2]
 
-    get_file_path = os.path.split(req_path)[1]
-    get_file_extension = os.path.splitext(get_file_path)[1]
+#     get_file_path = os.path.split(req_path)[1]
+#     get_file_extension = os.path.splitext(get_file_path)[1]
         
-    try:
-        with open(get_file_path) as fp:
-            data = fp.read()   
-            data_length = data.encode("ISO-8859-1")
-            length = len(data_length)
-            response = ("HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}").format(extensions[get_file_extension], length, data)
-            new_socket.sendall(response.encode("ISO-8859-1"))
-    except:
-           response = ("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 13\r\nConnection: close\r\n\r\n404 not found")
-           new_socket.sendall(response.encode("ISO-8859-1"))
-    new_socket.close()
+#     try:
+#         with open(get_file_path) as fp:
+#             data = fp.read()   
+#             data_length = data.encode("ISO-8859-1")
+#             length = len(data_length)
+#             response = ("HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}").format(extensions[get_file_extension], length, data)
+#             new_socket.sendall(response.encode("ISO-8859-1"))
+#     except:
+#            response = ("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 13\r\nConnection: close\r\n\r\n404 not found")
+#            new_socket.sendall(response.encode("ISO-8859-1"))
+#     new_socket.close()
